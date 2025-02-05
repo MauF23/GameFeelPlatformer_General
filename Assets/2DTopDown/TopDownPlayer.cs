ea1f1ade8f1a790f89810843721c93e5eaa21e17
F.MG2099 <f.mandujano@saei.mx>
2025-02-05 07:11:52 -0600

    TopDownScene

--- Assets/2DTopDown/TopDownPlayer.cs
+++ Assets/2DTopDown/TopDownPlayer.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 
 public class TopDownPlayer : MonoBehaviour
 {
-    public float movementSpeed;
-    public Rigidbody2D rigidbody;
-    public Animator animator;
+	public float movementSpeed;
+	public Rigidbody2D rigidbody;
+	public Animator animator;
 
-    private Vector2 movementVector;
+	private Vector2 movementVector;
 	private Vector2 lastMovementVector;
 
-    private const string xAxis = "Horizontal";
-    private const string yAxis = "Vertical";
+	private const string xAxis = "Horizontal";
+	private const string yAxis = "Vertical";
 	private const string ANIM_MOVE_SPEED = "moveSpeed";
 	private const string ANIM_MOVE_DIRECTIONX = "moveDirectionX";
 	private const string ANIM_MOVE_DIRECTIONY = "moveDirectionY";
 
 
 	void Start()
-    {
-        
-    }
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        movementVector = new Vector2(Input.GetAxis(xAxis), Input.GetAxis(yAxis)).normalized;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		movementVector = new Vector2(Input.GetAxis(xAxis), Input.GetAxis(yAxis)).normalized;
 	}
 
 	private void LateUpdate()
@@ -42,8 +42,8 @@ public class TopDownPlayer : MonoBehaviour
 			lastMovementVector = rigidbody.velocity.normalized;
 		}
 
-		animator.SetFloat(ANIM_MOVE_SPEED, rigidbody.velocity.sqrMagnitude);
-		animator.SetFloat(ANIM_MOVE_DIRECTIONX, lastMovementVector.x);
-		animator.SetFloat(ANIM_MOVE_DIRECTIONY, lastMovementVector.y);
+		animator?.SetFloat(ANIM_MOVE_SPEED, rigidbody.velocity.sqrMagnitude);
+		animator?.SetFloat(ANIM_MOVE_DIRECTIONX, lastMovementVector.x);
+		animator?.SetFloat(ANIM_MOVE_DIRECTIONY, lastMovementVector.y);
 	}
 }
