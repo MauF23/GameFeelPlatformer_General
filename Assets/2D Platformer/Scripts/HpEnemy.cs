7e6747ac9c7679715d6c0c07563f118f6fff78e9
F.MG2099 <f.mandujano@saei.mx>
2025-02-05 08:52:07 -0600

    TopDownAnimator

--- Assets/2D Platformer/Scripts/HpEnemy.cs
+++ Assets/2D Platformer/Scripts/HpEnemy.cs
@@ -1,9 +1,21 @@
+using DG.Tweening;
+using UnityEngine;
 
 public class HpEnemy : HpBase
 {
+	private float deathTweenTime = 0.3f;
+	public Rigidbody2D rigidbody;
+	public SpriteRenderer spriteRenderer;
 	protected override void Death()
 	{
 		base.Death();
+		rigidbody.velocity = Vector2.zero;
+		//transform.DOScale(0, deathTweenTime).OnComplete(VanishEnemy);
+		spriteRenderer.DOFade(0, deathTweenTime).OnComplete(VanishEnemy);
+	}
+
+	private void VanishEnemy()
+	{
 		gameObject.SetActive(false);
 	}
 }
